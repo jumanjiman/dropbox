ce33be08c8fdd7f4880015a251ec911ea205d72e
Paul Morgan <jumanjiman@gmail.com>
2015-05-17 14:37:12 -0400

    start vsftpd as pid 1

--- src/start.sh
+++ src/start.sh
@@ -3,6 +3,8 @@
 readonly logfile=/var/log/vsftpd.log
 tail -F $logfile &
 
+tail -F $logfile &
+
 touch $logfile
 chmod o+wx /var/ftp/pub/uploads
-/usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
+exec /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
